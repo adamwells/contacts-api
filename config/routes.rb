4adcfc447dadbb81a3e28698725721d860432a48
App Academy Student <student@appacademy.io>
2015-03-09 15:50:17 -0700

    Complete nested contact resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :destroy, :index, :show, :update] do
     resources :contacts, only: [:index]
   end
-  resources :contacts, :except [:index]
+  resources :contacts, except: [:index]
   resources :contact_shares, only: [:create, :destroy]
 end
